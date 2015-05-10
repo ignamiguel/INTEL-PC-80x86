b83012fa7f1dfa55ad29af927b44f6a903b72d71
ignamiguel <iglesiasnacho@gmail.com>
2015-05-10 05:19:05 -0300

    Convertion from 100 d to octal not working.

--- tpintel.asm
+++ tpintel.asm
@@ -6,10 +6,20 @@ segment datos data
 		        db 0
 		cadena  times 6 resb 1
 		
-		numero  db  0
+		octalNum times 10 resb 1  
 		
-		esMenor  db 'Es menor a 8','$'
+		caracter db  0
+		divisor  db  8 
+		numero  dw  0
+		
+		divdendo dw  0
+		
+		
+		esMenor  db 'ES 100!!!Es menor a 8','$'
  		esMayorIgual  db 'Es mayor o igual a 8','$'
+		
+		factor  db  1
+		index       db  0
 		
 		salto db 13,10,'$'
 		
@@ -26,6 +36,7 @@ segment codigo code
 		int 21h ;ingreso de cadena
 		
 		mov ax,0
+		;copia la longitud de los caracters ingresados
 		mov al,[cadena-1]
 		mov si,ax
 		mov byte[cadena+si],'$' ; piso el 0Dh con el '$'para indicar fin de string para imprimir
@@ -36,24 +47,117 @@ segment codigo code
 		lea dx,[cadena]		
 		call printMsg ; imprimo lo ingresado
 
-		call saltoDeLinea
+		call printEnter
 		
-		; copio el 1er caracter
-		mov  dl,byte[cadena]
-		mov  byte[numero],dl
-		; lo transformo a numero restando al primero nibble 30
-		sub  byte[numero],30h
+		; convierto caracteres a BPF c/s		
+		; y lo guardo el numero
+		call convertToBinary		
+		
+		; convierto a octal numero
+		call convertToOctal
+		
+		;muestro
+		lea  dx,[octalNum]
+		call printMsg
+		
 		;comparo con el 8
-		cmp  byte[numero],08h
-		jl   procesarMenor
+		;cmp  byte[numero],100
+		;je   procesarMenor		
 		
-		lea dx,[esMayorIgual]		
-		call printMsg ;imprimo mensaje
+		;lea dx,[esMayorIgual]		
+		;call printMsg ;imprimo mensaje
 		
 salir:		
 		mov ah,4ch
 		int 21h
 
+convertToBinary:
+        ; cargo la longitud de los caracteres ingresados
+		mov ax,0		
+		mov al,[cadena-1]
+		mov  cx,ax
+		
+		; uso una variable para ver por cuanto multiplicar
+		mov  byte[index],al
+		sub  byte[index],01h
+		
+doConvertion:		
+		; copio de atras para adelante
+		mov ax,0
+		mov al,byte[index]
+		mov si,ax
+		mov dl,byte[cadena+si]
+				
+		mov  byte[caracter],dl
+		
+		; lo transformo a numero restando al primero nibble 30
+		sub  byte[caracter],30h
+		
+		;multiplico por factor
+		mov  al,byte[factor]
+		mul  byte[caracter]
+		add  word[numero],ax
+		
+		; ajusto el factor multiplicando
+		mov ax,0
+		mov al,10
+		mul byte[factor]
+		mov byte[factor],al
+		
+		; resto 1 al indice
+		sub  byte[index],1
+		
+		loop doConvertion
+		
+		ret
+
+convertToOctal:
+        ; uso la variable indice para la posicon donde guardar
+		mov  byte[index],9	
+		
+nacho:
+		; cargo de atras para adelante
+		mov ax,0
+		mov al,byte[index]
+		mov si,ax
+			
+		;limpio el registro ax		
+		mov ax, 0
+		
+		;copio numero a ax
+		mov ax,word[numero]
+		
+		;divido por divisor=8
+		div byte[divisor]
+		
+		;guardo el resto en caracter
+		mov byte[octalNum+si],ah
+
+        ;le sumo 30h para convertirlo a ASCII
+     	add byte[octalNum+si],30h	
+		
+		;copio el cociente a numero
+		mov dx,0
+		mov dl,al
+        mov word[numero],dx
+		
+		;resto 1 a indice
+		sub  byte[index],1
+		
+		; comparo si el cociento contra la base=8
+		cmp word[numero],0008h		
+        jge nacho
+		
+		mov byte[octalNum+si],al
+		
+		;cierro el vector
+		mov ax,0
+		mov al,10
+		mov si,ax
+		mov byte[octalNum+si],'$'
+		
+		ret
+		
 procesarMenor:
 
 		lea dx,[esMenor]		
@@ -65,8 +169,9 @@ printMsg:
 		int 21h
 		ret
 		
-saltoDeLinea:
-    mov dx,salto			;realizo un salto de linea
-	mov ah,9h
-	int 21h
-	ret
+printEnter:
+		mov dx,salto			;realizo un salto de linea
+		mov ah,9h
+		int 21h
+		ret
+		
